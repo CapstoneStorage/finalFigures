b2267a1386ecc8efcc71fd70a277997d955e0726
jk0527 <78495599+jk0527@users.noreply.github.com>
2022-05-17 17:53:39 +0900

    Update workload_power_offloadingratio.m

--- workload_power_offloadingratio.m
+++ workload_power_offloadingratio.m
@@ -1,43 +1,25 @@
 close all;
 clear all;
 %% parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% directory %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+foldername = "C:\Users\user\Desktop\figures\220514";
+% mkdir(foldername);
 %% WORKLOAD POWER %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-workload = [0.1
+workload = [
 0.2
 0.3
 0.4
-0.5
-0.6
-0.7
+];
+power =[
+2.113982
+3.876613
+4.905618
+];
+offloading =[
+0.66
 0.8
-0.9
-1
-1.1];
-power =[0.477309
-0.817044
-1.493936
-2.006709
-2.524337
-3.348869
-3.281431
-7.188627
-12.248362
-23.655307
-46.597591];
-offloading =[0.87
-0.87
-0.8
-0.85
-0.76
-0.8
-0.82
-0.73
-0.62
-0.48
-0.19];
-%% directory %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-foldername = "../figures/220405";
-% mkdir(foldername);
+0.83
+];
 %% plot %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure(1)
 clf
@@ -45,37 +27,43 @@ hold on
 grid on
 box on
 X = categorical(workload);
+% ylim([0, 1.2])
+% power = power / 34.925583;
 xlabel('Workload');
 yyaxis left
+%yticks(0:0.2:1)
+%yticklabels(0:0.2:1)
 b = bar(X, power, 'FaceColor', [0.4 0.4 0.4]);
 % bar(X, cpufreq, 'stacked', 'CData', parula(4));
 xtips = b.XEndPoints;
 ytips = b.YEndPoints;
 ylabel('Power');
 set(gca,'fontsize', 12, 'YColor', 'k');
-ylab1 = get(gca, 'ylabel');
-set(ylab1, 'VerticalAlignment', 'middle');
-ylab1.Position(1) = -1;
+%ylab1 = get(gca, 'ylabel');
+%set(ylab1, 'VerticalAlignment', 'middle');
+%ylab1.Position(1) = -0.3;
 
 yyaxis right
+%yticks(0:0.2:1)
+%yticklabels(0:0.2:1)
 plot(X, offloading, 'Color', 'r', 'Marker', '*', 'Markersize', 7, 'LineWidth', 1);
-ylabel("Ratio");
+ylabel("Offloading Ratio");
 set(gca,'fontsize', 12, 'YColor', 'k');
-ylab2 = get(gca, 'ylabel');
-set(ylab2, 'VerticalAlignment', 'middle');
+%ylab2 = get(gca, 'ylabel');
+%set(ylab2, 'VerticalAlignment', 'middle');
 %set(ylab2, 'rotation', 270, 'VerticalAlignment', 'middle');
 %xh = get(gca, 'ylabel');
 %p = get(xh, 'position');
 %p(2) = 2*p(2);
 %set(xh, 'position', p);
-ylab2.Position(1) = 13.12;
+% ylim([0,1])
 
 
-legend('Power', 'Offloading Ratio','Location', 'northeast');
+legend('Power', 'Offloading Ratio','Location', 'northwest');
 hold off
 
 %title('COVS');
 
-saveas(figure(1),foldername+'/figure_workload_power_offloading_covs'+'.png');
-saveas(figure(1),foldername+'/figure_workload_power_offloading_covs'+'.fig');
-saveas(figure(1),foldername+'/figure_workload_power_offloading_covs'+'.pdf');
+saveas(figure(1),foldername+'/figure_workload_power_offloadingtest'+'.png');
+saveas(figure(1),foldername+'/figure_workload_power_offloadingtest'+'.fig');
+saveas(figure(1),foldername+'/figure_workload_power_offloadingtest'+'.pdf');
