87b22c8c0c678034ab9936d8fe5557653ddd9879
jk0527 <78495599+jk0527@users.noreply.github.com>
2022-05-17 17:36:22 +0900

    Update workload_cpufreq.m

--- workload_cpufreq.m
+++ workload_cpufreq.m
@@ -1,32 +1,22 @@
 close all;
 clear all;
 %% parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% directory %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+foldername = "C:\Users\user\Desktop\figures\220514";
+% mkdir(foldername);
 %% WORKLOAD POWER %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-workload = [0.1
+workload = [
 0.2
 0.3
 0.4
 0.5
-0.6
-0.7
-0.8
-0.9
-1
-1.1];
-cpufreq = [15,  15,  32,  38
-18,  13,  29,  40
-15,  27,  29,  29
-13,  22,  29,  36 
-18,  16,  23,  43
-13,  23,  29,  35
-20,  19,  28,  33
-28,  23,  30,  19 
-31,  26,  24,  19
-58,  23,  12,  7
-99,  1,  0,  0 ];
-%% directory %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-foldername = "../figures/220405";
-% mkdir(foldername);
+];
+cpufreq = [
+18 17 25 40 
+11 24 22 43 
+15 20 22 43 
+17 23 26 34 
+];
 %% plot %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure(1)
 clf
@@ -44,9 +34,9 @@ for i = 1:4
 end
 xtips = b.XEndPoints;
 ytips = b.YEndPoints;
-xlabel('workload');
+xlabel('Workload');
 
-plot(X, offloading);
+% plot(X, offloading);
 
 set(gca,'fontsize',12, 'YColor', 'k');
 legend('1', '0.5', '0.25', '0.125','Location', 'southeast');
@@ -54,8 +44,8 @@ ylabel('CPU Frequency (%)');
 
 hold off
 
-title('COVS');
+title('Network: 80');
 
-saveas(figure(1),foldername+'/figure_workload_cpufreq_offloading_covs'+'.png');
-saveas(figure(1),foldername+'/figure_workload_cpufreq_offloading_covs'+'.fig');
-saveas(figure(1),foldername+'/figure_workload_cpufreq_offloading_covs'+'.pdf');
+saveas(figure(1),foldername+'/figure_workload_cpufreq'+'.png');
+saveas(figure(1),foldername+'/figure_workload_cpufreq'+'.fig');
+saveas(figure(1),foldername+'/figure_workload_cpufreq'+'.pdf');
