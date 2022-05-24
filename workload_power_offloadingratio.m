6c2b13d648be437dda3762cf1d352b15be4e5c13
jk0527 <78495599+jk0527@users.noreply.github.com>
2022-05-24 09:11:38 +0900

    Update workload_power_offloadingratio.m

--- workload_power_offloadingratio.m
+++ workload_power_offloadingratio.m
@@ -2,23 +2,19 @@ close all;
 clear all;
 %% parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% directory %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-foldername = "C:\Users\user\Desktop\figures\220514";
+foldername = "C:\Users\user\Desktop\figures\220517\final";
 % mkdir(foldername);
 %% WORKLOAD POWER %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 workload = [
 0.2
-0.3
-0.4
 ];
 power =[
-2.113982
-3.876613
-4.905618
+2.7786972
+4.4289406
 ];
 offloading =[
-0.66
-0.8
-0.83
+0.723
+0.73
 ];
 %% plot %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure(1)
@@ -28,17 +24,17 @@ grid on
 box on
 X = categorical(workload);
 % ylim([0, 1.2])
-% power = power / 34.925583;
+power = power / 13.9278219;
 xlabel('Workload');
 yyaxis left
-%yticks(0:0.2:1)
-%yticklabels(0:0.2:1)
+yticks(0:0.2:1)
+yticklabels(0:0.2:1)
 b = bar(X, power, 'FaceColor', [0.4 0.4 0.4]);
 % bar(X, cpufreq, 'stacked', 'CData', parula(4));
 xtips = b.XEndPoints;
 ytips = b.YEndPoints;
-ylabel('Power');
-set(gca,'fontsize', 12, 'YColor', 'k');
+ylabel('Relative Energy');
+set(gca,'fontsize', 14, 'YColor', 'k');
 %ylab1 = get(gca, 'ylabel');
 %set(ylab1, 'VerticalAlignment', 'middle');
 %ylab1.Position(1) = -0.3;
@@ -46,9 +42,9 @@ set(gca,'fontsize', 12, 'YColor', 'k');
 yyaxis right
 %yticks(0:0.2:1)
 %yticklabels(0:0.2:1)
-plot(X, offloading, 'Color', 'r', 'Marker', '*', 'Markersize', 7, 'LineWidth', 1);
+plot(X, offloading, 'Color', 'r', 'Marker', '*', 'Markersize', 12, 'LineWidth', 1);
 ylabel("Offloading Ratio");
-set(gca,'fontsize', 12, 'YColor', 'k');
+set(gca,'fontsize', 14, 'YColor', 'k');
 %ylab2 = get(gca, 'ylabel');
 %set(ylab2, 'VerticalAlignment', 'middle');
 %set(ylab2, 'rotation', 270, 'VerticalAlignment', 'middle');
@@ -56,14 +52,15 @@ set(gca,'fontsize', 12, 'YColor', 'k');
 %p = get(xh, 'position');
 %p(2) = 2*p(2);
 %set(xh, 'position', p);
-% ylim([0,1])
+%ylim([0.6 0.9])
 
 
-legend('Power', 'Offloading Ratio','Location', 'northwest');
+lgd = legend('Power', 'Offloading Ratio','Location', 'northwest');
+set(lgd, 'fontsize', 14);
 hold off
 
 %title('COVS');
 
-saveas(figure(1),foldername+'/figure_workload_power_offloadingtest'+'.png');
-saveas(figure(1),foldername+'/figure_workload_power_offloadingtest'+'.fig');
-saveas(figure(1),foldername+'/figure_workload_power_offloadingtest'+'.pdf');
+saveas(figure(1),foldername+'/figure_workload_power_offloading'+'.png');
+saveas(figure(1),foldername+'/figure_workload_power_offloading'+'.fig');
+saveas(figure(1),foldername+'/figure_workload_power_offloading'+'.pdf');
