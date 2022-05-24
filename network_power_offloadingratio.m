0ee772f7d8ee4c0928addcf9da294db80aed736c
jk0527 <78495599+jk0527@users.noreply.github.com>
2022-05-24 09:11:01 +0900

    Update network_power_offloadingratio.m

--- network_power_offloadingratio.m
+++ network_power_offloadingratio.m
@@ -2,28 +2,19 @@ close all;
 clear all;
 %% parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% directory %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-foldername = "C:\Users\user\Desktop\figures\220514";
+foldername = "C:\Users\user\Desktop\figures\220517\final";
 % mkdir(foldername);
 %% WORKLOAD POWER %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 network = [
-10
-20
 30
 40
-50
 ];
 power =[
-21.147943
-15.880218
-11.91594
-9.928706
-8.03074
+18.1986867
+15.0796942
 ];
 offloading =[
-0.37
-0.55
-0.67
-0.66
+0.73
 0.76
 ];
 %% plot %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -34,16 +25,16 @@ grid on
 box on
 X = categorical(network);
 xlabel('Network (Mbps)');
-%power = power / 23.592769;
+power = power / 18.1986867;
 yyaxis left
-%yticks(0:0.2:1)
-%yticklabels(0:0.2:1)
+yticks(0:0.2:1)
+yticklabels(0:0.2:1)
 b = bar(X, power, 'FaceColor', [0.4 0.4 0.4]);
 % bar(X, cpufreq, 'stacked', 'CData', parula(4));
 xtips = b.XEndPoints;
 ytips = b.YEndPoints;
-ylabel('Power');
-set(gca,'fontsize', 12, 'YColor', 'k');
+ylabel('Relative Energy');
+set(gca,'fontsize', 14, 'YColor', 'k');
 %ylab1 = get(gca, 'ylabel');
 %set(ylab1, 'VerticalAlignment', 'middle');
 %ylab1.Position(1) = -1;
@@ -51,9 +42,9 @@ set(gca,'fontsize', 12, 'YColor', 'k');
 yyaxis right
 %yticks(0:0.2:1)
 %yticklabels(0:0.2:1)
-plot(X, offloading, 'Color', 'r', 'Marker', '*', 'Markersize', 7, 'LineWidth', 1);
-ylabel("Offloading Ratio");
-set(gca,'fontsize', 12, 'YColor', 'k');
+plot(X, offloading, 'Color', 'r', 'Marker', '*', 'Markersize', 12, 'LineWidth', 1);
+ylabel('Offloading Ratio');
+set(gca,'fontsize', 14, 'YColor', 'k');
 %ylab2 = get(gca, 'ylabel');
 %set(ylab2, 'VerticalAlignment', 'middle');
 % ylim([0.1 1.0]);
@@ -65,10 +56,12 @@ set(gca,'fontsize', 12, 'YColor', 'k');
 %ylab2.Position(1) = 13.7;
 %ylim([0 1])
 
-legend('Power', 'Offloading Ratio','Location', 'northwest');
+lgd = legend('Power', 'Offloading Ratio','Location', 'northwest');
+set(lgd, 'fontsize', 14);
+set(gca,'fontsize', 14);
 hold off
 
-title('Workload: 0.5');
+%title('Workload: 0.5');
 
 saveas(figure(1),foldername+'/figure_network_power_offloading'+'.png');
 saveas(figure(1),foldername+'/figure_network_power_offloading'+'.fig');
