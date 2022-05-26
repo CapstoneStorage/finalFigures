c7eb6bddca36d0731146a0746e2ffff6ce17fd69
jk0527 <78495599+jk0527@users.noreply.github.com>
2022-05-26 18:18:44 +0900

    Update network_power_algorithm.m

--- network_power_algorithm.m
+++ network_power_algorithm.m
@@ -4,47 +4,32 @@ clear all;
 %% TRIALS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 trial = 1;
 %% directory %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-foldername = "C:\Users\user\Desktop\figures\220514";
+foldername = "C:\Users\user\Desktop\figures\220517\final";
 % mkdir(foldername);
 %% NETWORK %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 network = [
-10
-20
 30
 40
-50
 ];
 %% OURS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Ours = [
-21.147943
-15.880218
-11.91594
-9.928706
-8.03074
+18.1986867
+15.0796942
 ];
 %% OFFLOADING %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Offloading = [
-27.383554
-19.905769
-16.05967
-13.830458
-12.52954
+23.0477133
+19.2296799
 ];
 %% DVFS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 DVFS = [
-21.208926
-21.208926
-21.208926
-21.208926
-21.208926
+34.2153286
+34.2153286
 ];
 %% DEFAULT %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 D = [
-26.583037
-26.583037
-26.583037
-26.583037
-26.583037
+37.4118328
+37.4118328
 ];
 %% plot %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure(1)
@@ -54,25 +39,25 @@ grid on
 box on
 X = categorical(network);
 
-%Ours = Ours / 31.527809;
-%D = D / 31.527809;
-%Offloading =  Offloading / 31.527809;
-%DVFS = DVFS / 31.527809;
+Ours = Ours / 37.4118328;
+D = D / 37.4118328;
+Offloading =  Offloading / 37.4118328;
+DVFS = DVFS / 37.4118328;
 
-plot(X, Ours, 'Color', 'r', 'Marker', '*', 'Markersize', 7, 'LineWidth', 1);
-plot(X, D, 'Color', 'k', 'Marker', 'o', 'Markersize', 7, 'LineWidth', 1);
-plot(X, DVFS, 'Color', 'b', 'Marker', '^', 'Markersize', 7, 'LineWidth', 1);
-plot(X, Offloading, 'Color', 'g', 'Marker', 'diamond', 'Markersize', 7, 'LineWidth', 1);
+plot(X, Ours, 'Color', 'r', 'Marker', '*', 'Markersize', 9, 'LineWidth', 1);
+plot(X, Offloading, 'Color', 'g', 'Marker', 'diamond', 'Markersize', 9, 'LineWidth', 1);
+plot(X, DVFS, 'Color', 'b', 'Marker', '^', 'Markersize', 9, 'LineWidth', 1);
+plot(X, D, 'Color', 'k', 'Marker', 'o', 'Markersize', 9, 'LineWidth', 1);
 
 % xlim([0 1]);
-% ylim([0 60]);
+ylim([0 1.2]);
 
-legend('ECVS', 'Default', 'DVFS', 'Offloading', 'Location','northeast', 'NumColumns', 2);
-
+lgd = legend('TOVS','Offloading', 'DVFS', 'Default', 'Location','northwest', 'NumColumns', 1);
+set(lgd, 'fontsize', 14)
 xlabel("Network (Mbps)");
-ylabel('Power');
+ylabel('Relative Energy');
 
-set(gca,'fontsize',12);
+set(gca,'fontsize',14);
 % title('Power');
 
 saveas(figure(1),foldername+'/figure_network_power_algorithm'+'.png');
