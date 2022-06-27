2a24577ca5fd88616764a9e22d24d6a60d029729
jk0527 <78495599+jk0527@users.noreply.github.com>
2022-06-27 21:24:37 +0900

    Update workload_power_algorithm.m

--- workload_power_algorithm.m
+++ workload_power_algorithm.m
@@ -4,32 +4,27 @@ clear all;
 %% TRIALS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 trial = 1;
 %% directory %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-foldername = "C:\Users\user\Desktop\figures\220517\final";
+foldername = "C:\Users\User\Desktop\figures\220626";
 % mkdir(foldername);
 %% WORKLOAD %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 workload = [
 0.2
-0.3
 ];
 %% OURS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Ours = [
 2.7786972
-4.4289406
     ];
 %% OFFLOADING %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Offloading = [
 4.9359804
-6.8695846
 ];
 %% DVFS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 DVFS = [
 5.3697136
-8.9835099
 ];
 %% DEFAULT %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 D = [
 9.5279598
-15.3098447
 ];
 %% plot %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure(1)
@@ -38,10 +33,10 @@ hold on
 grid on
 box on
 X = categorical(workload);
-Ours = Ours / 48.8730934;
-D = D / 48.8730934;
-Offloading =  Offloading / 48.8730934;
-DVFS = DVFS / 48.8730934;
+Ours = Ours/48.8730934;
+D = D/48.8730934;
+Offloading =  Offloading/48.8730934;
+DVFS = DVFS/48.8730934;
 plot(X, Ours, 'Color', 'r', 'Marker', '*', 'Markersize', 9, 'LineWidth', 1);
 plot(X, Offloading, 'Color', 'g', 'Marker', 'diamond', 'Markersize', 9, 'LineWidth', 1);
 plot(X, DVFS, 'Color', 'b', 'Marker', '^', 'Markersize', 9, 'LineWidth', 1);
@@ -50,7 +45,7 @@ plot(X, D, 'Color', 'k', 'Marker', 'o', 'Markersize', 9, 'LineWidth', 1);
 % xlim([0 1]);
 % ylim([0 60]);
 
-lgd = legend('TOVS', 'Offloading', 'DVFS', 'Default', 'Location','northwest');
+lgd = legend('Co-TOMS', 'Offloading', 'DVFS', 'Baseline', 'Location','northwest');
 set(lgd, 'fontsize', 14)
 xlabel("Workload");
 ylabel('Relative Energy');
